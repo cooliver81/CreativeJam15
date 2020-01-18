f3b30a60173e9de043f155ec6fa765a6046a3512
Nad Jam <nadjam@nad.qc.ca>
2020-01-18 13:06:52 -0500

    gameplay functional

--- Turntacle/Assets/Scenes/scripts/player.cs
+++ Turntacle/Assets/Scenes/scripts/player.cs
@@ -8,15 +8,16 @@ public class Player
     //List<List<int>> allRounds = new List<List<int>>(new List<int>(new int[3]));
     //List<int> round = new List<int>(new int[3]); //list of 0s
 
-    const int ROUND = 3;
+    const int ROUND = 5;
     const int MOVE = 3;
     const double STARTINGSTATS = 20;
-    const double STARTINGHEALTH = 50;
-    const double STARTINGDEDUCT = 1;
+    const double STARTINGHEALTH = 100;
+    const double STARTINGDEDUCT = 2;
     const int START = 0;
-    double PENALTYPERCENTAGE = 1.5; //Multiplication to reduce more stats if repeated
+    const double PENALTYPERCENTAGE = 1.5; //Multiplication to reduce more stats if repeated
+    const double CHARGEMULT = 5;
 
-    int[,] matrix = new int[MOVE, ROUND]; // each player has a matrix to keep track of each move for each round
+    public int[,] matrix = new int[MOVE, ROUND]; // each player has a matrix to keep track of each move for each round
     enum Moves
     {
         Nothing,
@@ -28,16 +29,17 @@ public class Player
     public int round;
     public int move;
 
-    public double attack;
-    public double dodge;
-    public double charge;
-    public double health;
+    public double attack = STARTINGSTATS;
+    public double dodge = STARTINGSTATS;
+    public double charge = 0;
+    public double health = STARTINGHEALTH;
 
-    public double attackDeduct;
-    public double dodgeDeduct;
+    public double attackDeduct = STARTINGDEDUCT;
+    public double dodgeDeduct = STARTINGDEDUCT;
 
     public bool penalty;
     public bool isCharged;
+
 
     // Start is called before the first frame update
     void Start()
@@ -71,34 +73,43 @@ public class Player
 
     public double Attack()
     {
-        isCharged = false;
+        double currentAttack = attack;
+        currentAttack += charge * CHARGEMULT;
         double deduct = attackDeduct;
         if (penalty) { deduct *= PENALTYPERCENTAGE; }
         attack = (attack - attackDeduct);
-        return attack;
+        charge = 0;
+        return currentAttack;
     }
     public bool Dodge()
     {
-        dodge--;
+        double deduct = dodgeDeduct;
+        if (penalty) { deduct *= PENALTYPERCENTAGE; }
         bool dodgeSuccess = true;
         var chance = Random.Range(1, 20);
-        if (chance > dodge) dodgeSuccess = false; 
+        if (chance > dodge) dodgeSuccess = false;
+        dodge = (dodge - deduct);
         return dodgeSuccess;
     }
     public void Charge()
     {
         //increase charge bar
-        isCharged = true;
+        
         charge++;
     }
 
     public void inputMove(int currentMove)
     {
-        Debug.Log(currentMove);
-        matrix[move, round] = currentMove;
-        if (currentMove == (int)Moves.Attack) Attack();
-        else if (currentMove == (int)Moves.Dodge) Dodge();
-        else if (currentMove == (int)Moves.Charge) Charge();
-        nextMove();
+        if (move < 3)
+        {
+            Debug.Log(currentMove);
+            matrix[move, round] = currentMove;
+            move++;
+        }
+        else
+        {
+            Debug.Log("out of moves!");
+        }
+
     }
 }
